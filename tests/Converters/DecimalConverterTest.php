9ba038971bf8f26ad41158210bf7b3e22171a2a7
Diego Calle <dnetix@gmail.com>
2016-05-04 23:47:36 -0500

    Tests to verify the Decimal to Romans converter

---
+++ tests/Converters/DecimalConverterTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use Dnetix\Converters\DecimalConverter;
+
+class DecimalConverterTest extends TestCase
+{
+
+    public function convertDecimal($decimal)
+    {
+        return DecimalConverter::load($decimal)->toRomans();
+    }
+
+    public function testItParsesCorrectlySimpleNumbers()
+    {
+        $this->assertEquals("MCMXX", $this->convertDecimal(1920));
+        
+        $this->assertEquals("LVIII", $this->convertDecimal(58));
+        
+        $this->assertEquals("IX", $this->convertDecimal(9));
+        
+        $this->assertEquals("I", $this->convertDecimal(1));
+    }
+
+    public function testItParsesIntegersPassedAsStrings()
+    {
+        $this->assertEquals("MMMMMDCCCLXXVI", $this->convertDecimal('5876'));
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testItThrowsExceptionWhenNoNumberPassed()
+    {
+        DecimalConverter::load('ABCD');
+    }
+
+}
